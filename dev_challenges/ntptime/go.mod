99aa60415df20675db5158794e713758fec441da
YawKar <yawkarpov@gmail.com>
2024-01-02 09:29:10 +0000

    solution(chllngs): ntptime

--- dev_challenges/ntptime/go.mod
+++ dev_challenges/ntptime/go.mod
@@ -1,3 +1,9 @@
 module github.com/yawkar/wbl2/dev_challenges/ntptime
 
 go 1.21.5
+
+require (
+	github.com/beevik/ntp v1.3.1 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+)
--- dev_challenges/ntptime/ntptime.go
+++ dev_challenges/ntptime/ntptime.go
@@ -1,3 +1,28 @@
+/*
+Russian ntp servers: https://www.ntp-servers.net/servers.html
+*/
 package main
 
-func main() {}
+import (
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
+func main() {
+	ntpAddress := flag.String("s", "ntp0.ntp-servers.net", "set ntp server's address")
+	timeout := flag.Duration("t", 30*time.Second, "set timeout")
+	flag.Parse()
+
+	options := ntp.QueryOptions{Timeout: *timeout}
+	response, err := ntp.QueryWithOptions(*ntpAddress, options)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	time := time.Now().Add(response.ClockOffset)
+	fmt.Println(time)
+}
